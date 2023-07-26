51e1493b51ad59e73d903fe4cc43ab7d4f12084b
GopiEmmadi <veeravenkatagopi@gmail.com>
2023-07-26 13:08:16 +0530

    angularday2

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,12 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
-  userName: string = 'John Doe';
-  userAge: number = 30;
-  userEmail: string = 'johndoe@example.com';
-
+  @ViewChild('inputField') inputField?: ElementRef;
+  userName: string = 'sensei';
+  userAge: number = 40;
+  userEmail: string = 'kshk@gmail.com';
+  flagName: boolean=false;
+  flagAge: boolean=false;
+  flagEmail: boolean=false;
   changeName(): void {
     // Implement the logic to change the user's name here
-    this.userName = 'Jane Smith';
+    this.userName = '';
+
+    if (this.inputField?.nativeElement) {
+      this.inputField.nativeElement.focus();
+    }
+  }
+  onName(){
+   if (!this.userName) {
+      this.userName='';
+      this.flagName=true;
+   }
+   else {
+      this.flagName=false;
+   }
+  }
+  onAge(){
+   if (!this.userAge) {
+      this.userAge=0;
+      this.flagAge=true;
+   }
+   else {
+      this.flagAge=false;
+   }
+  }
+  onChange(){
+   if (!this.userEmail.endsWith('@gmail.com')) {
+      this.userEmail='';
+      this.flagEmail=true;
+   }
+   else {
+      this.flagEmail=false;
+   }
   }
 }
